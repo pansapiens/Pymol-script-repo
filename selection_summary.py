9e0a8e20bc2d5c9b371fbd57fc67b84562a7d924
Andrew Perry <ajperry@pansapiens.com>
2012-05-30 17:40:10 +1000

    Added as_script feature to dump out selection commands.

--- selection_summary.py
+++ selection_summary.py
@@ -14,9 +14,9 @@ Date    : May 2012
     PyMoL> selection_summary offset=10
     
     Outputs:
-    selectionA: 15-26
-    selectionB: 45-79
-    sele: 101-259
+    # selectionA: 15-26
+    # selectionB: 45-79
+    # sele: 101-259
     
     Useful when you have manually selected a set of named sequence ranges 
     (loopA, loopB, etc) and want to document them outside of Pymol.
@@ -33,6 +33,10 @@ Date    : May 2012
             If all_residues=True, return every residue in the selection, not
             just the lowest and highest residue number.
             DEFAULT: False
+        
+        as_script (boolean)
+            Outputs selection commands that could recreate the selections 
+            by residue. Assumes each selection encompasses only one object/model.
 
     RETURNS
         A dictionary of the format { selection_name : (i_resi, j_resi) }, 
@@ -47,24 +51,46 @@ import sets
 def strTrue(p):
   return p[0].upper() == "T"
     
-def selection_summary(offset=0, all_residues="False"):
+def selection_summary(offset=0, all_residues="False", as_script="False"):
   offset=int(offset)
   returndic = {}
   for sel in cmd.get_names("selections"):
     s = sets.Set()
+    # get the residue numbers for this selection
     cmd.iterate(sel, 's.add(resi)', space={'s':s})
+    
+    # hack to get the model name. there must be a better way
+    mod = []
+    cmd.iterate(sel, 'mod.append(model)', space={'mod':mod})
+    try:
+      mod = mod[0] + " and"
+    except IndexError:
+      mod = ""
+    ##
+    
     l = list(s)
     l.sort()
     resints = map(int, l)
-    if resints and not strTrue(all_residues):
+    # TODO: (this logic would be better as a 
+    #        switch/case now that it's more complex)
+    if resints and not strTrue(all_residues) and not strTrue(as_script):
       maxresi = max(resints)
       minresi = min(resints)
-      print "%s: %i-%i" % (sel, minresi+offset, maxresi+offset)
+      print "# %s: %i-%i" % (sel, minresi+offset, maxresi+offset)
       returndic[sel] = (minresi+offset, maxresi+offset)
-    elif strTrue(all_residues):
-      print sel + ": " + ", ".join(l)
+    elif strTrue(all_residues) and not strTrue(as_script):
+      print "# " + sel + ": " + ", ".join(l)
       returndic[sel] = resints
-
+    elif strTrue(as_script):
+      if strTrue(all_residues):
+        print "select %s, %s " % (sel, mod) + "i. " + " i. ".join(l)
+        returndic[sel] = resints
+      elif resints:
+        maxresi = max(resints)
+        minresi = min(resints)
+        print "select %s, %s i. %i-%i" % (sel, mod, minresi, maxresi)
+        returndic[sel] = (minresi+offset, maxresi+offset)
+      
   return returndic
     
 cmd.extend('selection_summary', selection_summary)
